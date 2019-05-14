e254f6036e5867b3b1a4152fe5c771ece5130fa6
Dimitris Gravvanis <dgravvanis@icloud.com>
2019-05-14 03:03:08 +0300

    Release simple chain version 1.0.0

--- LevelSandbox.js
+++ LevelSandbox.js
@@ -2,40 +2,96 @@
 |  Learn more: level: https://github.com/Level/level |
 /===================================================*/
 
-const level = require('level');
-const chainDB = './chaindata';
+const level = require('level')
+const chainDB = './chaindata'
 
 class LevelSandbox {
 
-    constructor() {
-        this.db = level(chainDB);
-    }
+  constructor() {
+    this.db = level(chainDB)
+  }
 
-    // Get data from levelDB with key (Promise)
-    getLevelDBData(key){
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember in Promises you need to resolve() or reject()
-        });
-    }
+  // Promise => Block Object
+  // Get data from levelDB with key
+  // Returns Block Object or null
+  getLevelDBData(key) {
 
-    // Add data to levelDB with key and value (Promise)
-    addLevelDBData(key, value) {
-        let self = this;
-        return new Promise(function(resolve, reject) {
-            // Add your code here, remember in Promises you need to resolve() or reject() 
-        });
-    }
+    let self = this
+    // Return promise
+    return new Promise(function(resolve, reject) {
+      // Get block
+      self.db.get(key, function(err, value) {
+        // If block not found resolve with null
+        if (err instanceof level.errors.NotFoundError) {
+          resolve(null)
+        //} else if (err instanceof level.errors.LevelUPError) {
+          //console.log(err);
+          //reject(err)
+        } else {
+          resolve(JSON.parse(value))
+        }
+      })
+    })
+  }
 
-    // Method that return the height
-    getBlocksCount() {
-        let self = this;
-        return new Promise(function(resolve, reject){
-            // Add your code here, remember in Promises you need to resolve() or reject()
-        });
-    }
-        
+  // Promise
+  // Add data to levelDB with key and value
+  addLevelDBData(key, value) {
 
+    let self = this
+    return new Promise(function(resolve, reject) {
+        self.db.put(key, value, function(err) {
+          if (err) {
+            reject(console.log('Ooops!', err))
+          } else {
+            resolve(value, console.log('Block '+ key +' SAVED '+' => '))
+          }
+        })
+    })
+  }
+
+  // Promise => Number
+  // Returns the height of the chain
+  getBlocksCount() {
+
+    let self = this
+    return new Promise(function(resolve, reject) {
+      let dataArray = []
+      self.db.createReadStream()
+      .on('data', function (data) {
+        dataArray.push(data)
+      })
+      .on('error', function (err) {
+        reject(console.log('Ooops!', err))
+      })
+      .on('close', function () {
+        resolve(dataArray.length)
+      })
+    })
+  }
+
+  // Promise
+  // Deletes all data from storage
+  deleteAllData() {
+
+    let self = this
+    self.getBlocksCount()
+    .then(function(result) {
+
+      let ops = []
+      let i = 0
+      while (i < result) {
+        ops.push({ type: 'del', key: i })
+        i++
+      }
+      return new Promise(function(resolve, reject) {
+        self.db.batch(ops, function(err) {
+          if (err) return reject(console.log('Ooops!', err))
+          resolve(console.log('All Blocks Were Deleted'))
+        })
+      })
+    })
+  }
 }
 
-module.exports.LevelSandbox = LevelSandbox;
+module.exports.LevelSandbox = LevelSandbox
--- simpleChain.js
+++ simpleChain.js
@@ -7,112 +7,147 @@ const Block = require('./Block.js');
 
 let myBlockChain = new BlockChain.Blockchain();
 
+
 setTimeout(function () {
-	console.log("Waiting...")
-}, 10000);
+	//myBlockChain.bd.deleteAllData()
+	console.log('Waiting...')
+}, 100);
 
 /******************************************
  ** Function for Create Tests Blocks   ****
  ******************************************/
 
-
-(function theLoop (i) {
-	setTimeout(function () {
-		let blockTest = new Block.Block("Test Block - " + (i + 1));
-		// Be careful this only will work if your method 'addBlock' in the Blockchain.js file return a Promise
-		myBlockChain.addBlock(blockTest).then((result) => {
-			console.log(result);
-			i++;
-			if (i < 10) theLoop(i);
-		});
-	}, 10000);
-  })(0);
-
+ setTimeout(function () {
+	 (function theLoop(i) {
+ 		setTimeout(function() {
+ 			let blockTest = new Block.Block('Test Block - ' + (i + 1))
+ 			// Be careful this only will work if your method 'addBlock' in the Blockchain.js file return a Promise
+ 			myBlockChain.addBlock(blockTest)
+ 			.then(function(result) {
+				console.log(result)
+ 				i++
+ 				if (i < 10) theLoop(i)
+ 			})
+ 			.catch(function(err) {
+ 				console.log(err)
+ 			})
+ 		}, 1000)
+ 	})(0)
+ }, 2000);
 
 /***********************************************
  ** Function to get the Height of the Chain ****
  ***********************************************/
 
-/*
-// Be careful this only will work if `getBlockHeight` method in Blockchain.js file return a Promise
-myBlockChain.getBlockHeight().then((height) => {
-	console.log(height);
-}).catch((err) => { console.log(err);});
-*/
+ setTimeout(function () {
+	 // Be careful this only will work if `getBlockHeight` method in Blockchain.js file return a Promise
+	 myBlockChain.getBlockHeight()
+	 .then((height) => {
+	 		console.log('The height of the chain is ' + height + '.')
+	 })
+	 .catch((err) => {
+		 console.log(err)
+	 })
+ }, 22000);
 
 /***********************************************
  ******** Function to Get a Block  *************
  ***********************************************/
 
-/*
-// Be careful this only will work if `getBlock` method in Blockchain.js file return a Promise
-myBlockChain.getBlock(0).then((block) => {
-	console.log(JSON.stringify(block));
-}).catch((err) => { console.log(err);});
-*?
+ setTimeout(function () {
+	 // Be careful this only will work if `getBlock` method in Blockchain.js file return a Promise
+	 myBlockChain.getBlock(0)
+	 .then((block) => {
+		 console.log(JSON.stringify(block))
+	 })
+	 .catch((err) => {
+		 console.log(err)
+	 })
+ }, 23000);
+
 
 /***********************************************
  ***************** Validate Block  *************
  ***********************************************/
 
-/*
-// Be careful this only will work if `validateBlock` method in Blockchain.js file return a Promise
-myBlockChain.validateBlock(0).then((valid) => {
-	console.log(valid);
-})
-.catch((error) => {
-	console.log(error);
-})
-*/
+ setTimeout(function () {
+	 // Be careful this only will work if `validateBlock` method in Blockchain.js file return a Promise
+	 myBlockChain.validateBlock(0).then((valid) => {
+	 	console.log(valid);
+	 })
+	 .catch((error) => {
+	 	console.log(error);
+	 })
+ }, 24000);
 
 /** Tampering a Block this is only for the purpose of testing the validation methods */
-/*
-myBlockChain.getBlock(5).then((block) => {
-	let blockAux = block;
-	blockAux.body = "Tampered Block";
-	myBlockChain._modifyBlock(blockAux.height, blockAux).then((blockModified) => {
-		if(blockModified){
-			myBlockChain.validateBlock(blockAux.height).then((valid) => {
-				console.log(`Block #${blockAux.height}, is valid? = ${valid}`);
-			})
-			.catch((error) => {
-				console.log(error);
-			})
-		} else {
-			console.log("The Block wasn't modified");
-		}
-	}).catch((err) => { console.log(err);});
-}).catch((err) => { console.log(err);});
 
-myBlockChain.getBlock(6).then((block) => {
-	let blockAux = block;
-	blockAux.previousBlockHash = "jndininuud94j9i3j49dij9ijij39idj9oi";
-	myBlockChain._modifyBlock(blockAux.height, blockAux).then((blockModified) => {
-		if(blockModified){
-			console.log("The Block was modified");
-		} else {
-			console.log("The Block wasn't modified");
-		}
-	}).catch((err) => { console.log(err);});
-}).catch((err) => { console.log(err);});
+setTimeout(function () {
+	myBlockChain.getBlock(5).then((block) => {
+		let blockAux = block
+		blockAux.body = 'Tampered Block'
+		myBlockChain._modifyBlock(blockAux.height, blockAux)
+		.then((blockModified) => {
+			console.log(`Block #${blockAux.height} was modified.`)
+			if (blockModified) {
+				myBlockChain.validateBlock(blockAux.height)
+				.then((valid) => {
+					console.log(`Is #${blockAux.height} valid? => ${valid}`);
+				})
+				.catch((error) => {
+					console.log(error);
+				})
+			} else {
+				console.log('The Block was not modified')
+			}
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+	})
+	.catch((err) => {
+		console.log(err)
+	})
+}, 25000);
+
+setTimeout(function () {
+	myBlockChain.getBlock(6)
+	.then((block) => {
+		let blockAux = block
+		blockAux.previousBlockHash = 'jndininuud94j9i3j49dij9ijij39idj9oi'
+		myBlockChain._modifyBlock(blockAux.height, blockAux)
+		.then((blockModified) => {
+			if(blockModified){
+				console.log(`Block #${blockAux.height} was modified`)
+			} else {
+				console.log('The Block was not modified')
+			}
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+	})
+	.catch((err) => {
+		console.log(err)
+	})
+}, 27000);
 
 /***********************************************
  ***************** Validate Chain  *************
  ***********************************************/
 
-/*
-// Be careful this only will work if `validateChain` method in Blockchain.js file return a Promise
-myBlockChain.validateChain().then((errorLog) => {
-	if(errorLog.length > 0){
-		console.log("The chain is not valid:");
-		errorLog.forEach(error => {
-			console.log(error);
-		});
-	} else {
-		console.log("No errors found, The chain is Valid!");
-	}
-})
-.catch((error) => {
-	console.log(error);
-})
-*/
+setTimeout(function () {
+	// Be careful this only will work if `validateChain` method in Blockchain.js file return a Promise
+	myBlockChain.validateChain()
+	.then((errorLog) => {
+		if(errorLog.length > 0) {
+			console.log('The chain is not valid:')
+			errorLog.forEach(error => { console.log(error) })
+		} else {
+			console.log('No errors found, The chain is Valid!')
+		}
+	})
+	.catch((error) => {
+		console.log(error)
+	})
+}, 29000);
